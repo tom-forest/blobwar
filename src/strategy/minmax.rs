b8c2ec7224d865faa879f03c82e2f6042b5d3ed2
Sibylle Ducroize <sibylle.ducroize@grenoble-inp.org>
2023-03-02 22:32:28 +0100

    tmp

--- src/strategy/minmax.rs
+++ src/strategy/minmax.rs
@@ -9,7 +9,24 @@ pub struct MinMax(pub u8);
 
 impl Strategy for MinMax {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        unimplemented!("TODO: implementer min max")
+        let mut min_opponent_value: i8 = 127; //max value
+        let mut max_move : Option<Movement> = None;
+        let mut current_value : i8;
+        Greedy opponent;
+        for mov in state.movements() {
+            Configuration opponent_config = state.play(&mov);
+            Option<Movement> move_opponent = opponent.compute_next_move(opponent_config);
+            if move_opponent == None {
+                return mov //the value of the opponent added is 0 here 
+            }else{
+                current_value = opponent_config.play(move_opponent.unwrap()).value();
+                if current_value < min_opponent_value {
+                    max_move = mov;
+                    min_opponent_value = current_value;
+                }
+            }
+        }
+        return max_move;
     }
 }
 
